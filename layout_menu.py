8d6132cf106ba31b9379292f49ad4ff30cee92df
Yaroslav <20011aric@gmail.com>
2024-09-10 20:13:38 +0300

    додавання меню

---
+++ layout_menu.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QPushButton, QRadioButton, QLabel, QSpinBox, QGroupBox, QButtonGroup, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+
+btn_start = QPushButton("Почати тестування")
+btn_learning = QPushButton("Редагування питаннь")
+btn_exit = QPushButton("Вийти")
+
+main_line_menu = QVBoxLayout()
+main_line_menu.addWidget(btn_start, alignment=Qt.AlignCenter)
+main_line_menu.addWidget(btn_learning, alignment=Qt.AlignCenter)
+main_line_menu.addWidget(btn_exit, alignment=Qt.AlignCenter)
--- main.py
+++ main.py
@@ -3,21 +3,27 @@ from PyQt5.QtWidgets import QWidget, QApplication
 app = QApplication([])
 
 from layout_test import*
+from layout_menu import*
 
 window = QWidget()
 window_answer = QWidget()
+window_menu = QWidget()
 
 window.resize(600, 500)
 window_answer.resize(600, 500)
+window_menu.resize(200, 200)
 
 window.setWindowTitle('Memory Cards')
-window_answer.setWindowTitle('Memory Cards Answer')
+window_answer.setWindowTitle('Memory Cards-Answer')
+window_menu.setWindowTitle('Memory Cards-Menu')
 
 window.setLayout(main_line_test)
 window_answer.setLayout(main_line_answer)
+window_menu.setLayout(main_line_menu)
 
 window.show()
 window_answer.hide()
+window_menu.hide()
 
 def Answer():
     window.hide()
@@ -25,8 +31,13 @@ def Answer():
 def Next():
     window.show()
     window_answer.hide()
+def Menu():
+    window.hide()
+    window_answer.hide()
+    window_menu.show()
 
 btn_answer.clicked.connect(Answer)
 btn_next.clicked.connect(Next)
+btn_menu.clicked.connect(Menu)
 
 app.exec_()
