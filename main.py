9dabdf7fffed6773ced4ad8856846e18ed9e35fd
Yaroslav <20011aric@gmail.com>
2024-09-10 19:58:31 +0300

    перемикання між вікнами

--- main.py
+++ main.py
@@ -5,10 +5,28 @@ app = QApplication([])
 from layout_test import*
 
 window = QWidget()
-window.resize(600, 500)
-window.setWindowTitle('Memory Cards')
-window.setLayout(main_line_test)
-window.show()
+window_answer = QWidget()
 
+window.resize(600, 500)
+window_answer.resize(600, 500)
+
+window.setWindowTitle('Memory Cards')
+window_answer.setWindowTitle('Memory Cards Answer')
+
+window.setLayout(main_line_test)
+window_answer.setLayout(main_line_answer)
+
+window.show()
+window_answer.hide()
+
+def Answer():
+    window.hide()
+    window_answer.show()
+def Next():
+    window.show()
+    window_answer.hide()
+
+btn_answer.clicked.connect(Answer)
+btn_next.clicked.connect(Next)
 
 app.exec_()
